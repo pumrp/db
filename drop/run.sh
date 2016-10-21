3485f79d53e4426b6eca4a3d86a7dcd782585f8a
Julien Stroheker <julienstroheker@gmail.com>
2016-10-21 13:12:09 +0200

    Changing path of js

--- drop/run.sh
+++ drop/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo Starting Mongo Instance
+echo "******************* Starting Mongo Instance ********************"
 mongod --smallfiles --rest --fork --logpath /var/log/mongodb/server.log
-sleep 3
-echo Dumping Data
-mongo ordering /tmp/MongoRecords.js
+sleep 1
+echo "******************* Dumping Data ********************"
+mongo ordering /usr/local/app/MongoRecords.js
+echo "******************* Starting to log ********************"
 tail -f /var/log/mongodb/server.log
